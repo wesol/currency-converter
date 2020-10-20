d00ca1f32856ac9b73c84d65c61f8d3f4dbc7989
Mateusz Wesołowski <mwesolowski88@gmail.com>
2020-10-20 20:33:59 +0200

    Implemented logic

--- src/main/java/com/mw/cc/service/currencyConverters/CurrencyConverterNbp.java
+++ src/main/java/com/mw/cc/service/currencyConverters/CurrencyConverterNbp.java
@@ -1,14 +1,31 @@
 package com.mw.cc.service.currencyConverters;
 
+import com.mw.cc.service.currencyProviders.CurrencyProvider;
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.util.Map;
 
 @Service
+@AllArgsConstructor
+@Log4j2
 public class CurrencyConverterNbp implements CurrencyConverter {
+
+  private final CurrencyProvider provider;
 
   @Override
   public BigDecimal convert(BigDecimal amount, String originalCurrency, String targetCurrency) {
-    return null;
+
+    Map<String, BigDecimal> currencies = provider.getCurrencies();
+    if (currencies.containsKey(originalCurrency) && currencies.containsKey(targetCurrency)) {
+      BigDecimal divide = currencies.get(originalCurrency).divide(currencies.get(targetCurrency), MathContext.DECIMAL32);
+      return amount.multiply(divide);
+    } else {
+      log.info("No data for code: " + originalCurrency + " or: " + targetCurrency);
+      return new BigDecimal(-1);
+    }
   }
 }

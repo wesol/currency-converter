357674233cba871dadf5a3146d4734725333bfae
Mateusz Wesołowski <mwesolowski88@gmail.com>
2020-10-20 17:30:38 +0200

    Added project structure

---
+++ src/test/java/com/mw/cc/CurrencyConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mw.cc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConverterApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
